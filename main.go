5d9d1c396a755cf8b055caf028095f2a2a3a426a
suengguan <suengguan@126.com>
2017-05-09 17:38:27 +0800

    add print msg

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"fmt"
 	_ "hello-world/routers"
 
 	"github.com/astaxie/beego"
 )
 
 func main() {
-	fmt.Println("error message")
+	fmt.Println("start hello world service")
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
