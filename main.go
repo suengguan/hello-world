5bf3407511de42b57648105f2136d0cc0ffeb063
suengguan <suengguan@126.com>
2017-05-12 13:34:27 +0800

    print version msg

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	beego.Debug("this is hello world-v2")
+	beego.Debug("this is hello world-v5")
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
